95f904a590b262f616e20f4bd6cdef83a7d4d1a1
Khai Wei <121887080+ykhaiwei@users.noreply.github.com>
2024-10-04 22:54:41 +0800

    Update variable name from vg_1 to vg_2 in bubble plot script

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -5,8 +5,8 @@ vegaEmbed("#choropleth_map", vg_1)
   })
   .catch(console.error);
 
-var vg_1 = "bubble_plot.vg.json";
-vegaEmbed("#bubble_plot", vg_1)
+var vg_2 = "bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_2)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
